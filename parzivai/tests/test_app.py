87f11b4298469737215b9152f08fcf905359818c
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-04-29 08:28:22 +0200

    tests: make sure streamlit test is run

--- parzivai/tests/test_app.py
+++ parzivai/tests/test_app.py
@@ -1,18 +1,19 @@
 from streamlit.testing.v1 import AppTest
 import pytest
+
+
+app_script = """
+import streamlit as st
 from parzivai.app import main
+main()
+"""
 
 
 @pytest.fixture
 def get_app():
-    return AppTest.from_function(main)
+    return AppTest.from_string(app_script)
 
 
 def test_run_app(get_app):
-    get_app.run(timeout=10)
-    # momentarily, we cannot perform end-to-end testing
-    # the streamlit commands need to be all set in app.py
-    # currently, some are still set in the modules
-    # this should be further disentangled
-    # assert get_app.title == "ParzivAI"
-    # assert not get_app.exception
+    get_app.run(timeout=100)
+    assert not get_app.exception
